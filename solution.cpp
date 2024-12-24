12ff62d0bfef2bcb226ea8f4d64ca2bdf67fbaba
soneunsu <kekekee257@gmail.com>
2024-12-24 17:11:04 +0900

    도전과제 적용-정렬 기능 추가

--- solution.cpp
+++ solution.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -20,6 +21,36 @@ int getAvgScore(int array[], int size)
 	return total / size;
 }
 
+void sort(int array[], int size, int order = 1)
+{
+	cout << ((order == 1) ? "오름차순 정렬!" : "내림차순 정렬!") << endl;
+
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = 0; j < size - i - 1; j++)
+		{
+			if (order == 1)
+			{
+				if (array[j] > array[j + 1])
+				{
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+			else if (order == 2)
+			{
+				if (array[j] < array[j + 1])
+				{
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
+	}
+}
+
 int main()
 {
 	int scoreArray[5];
@@ -35,4 +66,29 @@ int main()
 	int avgScore = getAvgScore(scoreArray, 5);
 
 	cout << "5개 과목 점수의 총합은 " << totalScore << "점 이고 평균은 " << avgScore << "점 입니다." << endl;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "scoreArray[" << i << "] = " << scoreArray[i] << endl;
+	}
+
+	int order = 0;
+	while (true)
+	{
+		cout << "원하는 정렬 방향을 입력하세요 (오름차순 정렬-1, 내림차순 정렬-2, 종료-9)" << endl;
+		cin >> order;
+
+		if (order == 9)
+		{
+			cout << "프로그램 종료!!!" << endl;
+			break; 
+		}
+
+		sort(scoreArray, 5, order);
+	
+		for (int i = 0; i < 5; i++)
+		{
+			cout << "scoreArray[" << i << "] = " << scoreArray[i] << endl;
+		}
+	}
 }
